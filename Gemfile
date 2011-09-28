88854b4bafa1b96e7fb0c5b642344da127b46d9a
michael magz <michael.magner@gmail.com>
2011-09-28 14:16:34 -0400

    starting capistrano setup

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'json'
 gem "paperclip", "~> 2.4.2"
 gem "bhm-google-maps" 
 gem "gmaps4rails"
+gem "capistrano"
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "urbanthalon.fuzzproductions.com"
+set :repository,  "git@github.com:magz/urbanathalon.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :user, "michaelmagner"
+set :use_sudo, false
+set :deploy_to, "var/#{application}"
+set :deploy_via, :remote_cache
+
+role :web, "urbanathlon.fuzzproductions.com"                          # Your HTTP server, Apache/etc
+role :app, "urbanathlon.fuzzproductions.com"                          # This may be the same as your `Web` server
+role :db,  "urbanathlon.fuzzproductions.com", :primary => true # This is where Rails migrations will run
+role :db,  "urbanathlon.fuzzproductions.com"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+after "deploy", "deploy:bundle_gems"
+after "deploy:bundle:gems", "deploy:restart"
+
+
+# If you are using Passenger mod_rails uncomment this:
+ namespace :deploy do
+   task :bundle_gems do
+     run "cd #{deploy_to}/current && bundle install vendor/gems"
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
+ end
